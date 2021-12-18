54ad574ed5f318480dacf36a20ccb4acb62813dc
AMAN GUPTA <amangupta1995.ag@gmail.com>
2021-12-18 17:35:42 +0530

    Add files via upload

---
+++ test/test_sys/dl/test_od.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import torch
+from torch import nn
+import pickle as pkl
+from sklearn.metrics import accuracy_score
+
+sys.path.append(os.getcwd())
+from mindware.components.models.object_detection.yolov3 import Yolov3
+from mindware.datasets.od_dataset import ODDataset
+from mindware.estimators import ObjectionDetecter
+
+mode = 'fit'
+if mode == 'fit':
+    dataset = ODDataset('data/od_datasets/custom/custom.data')
+    clf = ObjectionDetecter(time_limit=30,
+                            include_algorithms=['yolov3'],
+                            ensemble_method='ensemble_selection')
+    clf.fit(dataset)
+
+    dataset.set_test_path('data/od_datasets/custom/valid.txt')
+    print(clf.predict(dataset))
+else:
+    config = Yolov3.get_hyperparameter_search_space().sample_configuration().get_dictionary()
+    config['epoch_num'] = 150
+    config['device'] = 'cuda'
+    clf = Yolov3(**config)
+
+    dataset = ODDataset('data/od_datasets/custom/custom.data')
+
+    dataset.load_data()
+    clf.fit(dataset)
+
+    dataset.set_test_path('data/od_datasets/custom/valid.txt')
+    dataset.load_test_data()
+    print(clf.predict(dataset.test_dataset))
+    print(clf.score(dataset, None))
