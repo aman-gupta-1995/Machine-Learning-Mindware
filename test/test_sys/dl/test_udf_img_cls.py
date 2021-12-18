8e8987bcae5efa4d065fbed7c8ccf06402540040
AMAN GUPTA <amangupta1995.ag@gmail.com>
2021-12-18 17:36:12 +0530

    Add files via upload

---
+++ test/test_sys/dl/test_udf_img_cls.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from torchvision import transforms
+from sklearn.metrics import accuracy_score
+
+sys.path.append(os.getcwd())
+
+from mindware.datasets.image_dataset import ImageDataset
+from mindware.estimators import ImageClassifier
+from mindware.components.models.img_classification import add_classifier
+from mindware.components.models.img_classification.resnext import ResNeXtClassifier
+
+# Add user-defined classifier
+add_classifier(ResNeXtClassifier)
+
+data_dir = 'data/img_datasets/hymenoptera_data/'
+image_data = ImageDataset(data_path=data_dir)
+clf = ImageClassifier(time_limit=7200,
+                      include_algorithms=['ResNeXtClassifier'],
+                      ensemble_method='ensemble_selection',
+                      config_file_path='tiny_cs.txt')
+clf.fit(image_data)
+image_data.set_test_path(data_dir)
+print(clf.predict_proba(image_data))
+print(clf.predict(image_data))
