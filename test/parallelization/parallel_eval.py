7507ee4be5a633dd1494d88f63ec5d69415dfb49
AMAN GUPTA <amangupta1995.ag@gmail.com>
2021-12-18 17:32:50 +0530

    Add files via upload

---
+++ test/parallelization/parallel_eval.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import argparse
+from ConfigSpace.hyperparameters import UnParametrizedHyperparameter
+
+sys.path.append(os.getcwd())
+from mindware.datasets.image_dataset import ImageDataset
+from mindware.components.optimizers.base.config_space_utils import sample_configurations
+from mindware.components.models.img_classification.nn_utils.nn_aug.aug_hp_space import get_aug_hyperparameter_space
+from mindware.components.evaluators.dl_evaluator import DLEvaluator
+from mindware.components.metrics.metric import get_metric
+from mindware.components.utils.constants import IMG_CLS
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--network', type=str, default='mobilenet')
+parser.add_argument('--dataset', type=str, default='extremely_small')
+args = parser.parse_args()
+
+data_dir = 'data/img_datasets/%s/' % args.dataset
+
+image_data = ImageDataset(data_path=data_dir, train_val_split=True)
+image_data.set_test_path(data_dir)
+evaluator = DLEvaluator(None,
+                        IMG_CLS,
+                        scorer=get_metric('acc'),
+                        dataset=image_data,
+                        device='cuda',
+                        image_size=32)
+
+
+from mindware.components.computation.parallel_process import ParallelProcessEvaluator
+from mindware.components.models.img_classification import _classifiers
+network_id = args.network
+config_space = _classifiers[network_id].get_hyperparameter_search_space()
+model = UnParametrizedHyperparameter("estimator", network_id)
+config_space.add_hyperparameter(model)
+aug_space = get_aug_hyperparameter_space()
+config_space.add_hyperparameters(aug_space.get_hyperparameters())
+config_space.add_conditions(aug_space.get_conditions())
+
+executor = ParallelProcessEvaluator(evaluator, n_worker=3)
+_configs = sample_configurations(config_space, 12)
+
+res = executor.parallel_execute(_configs, resource_ratio=0.1)
+print(res)
