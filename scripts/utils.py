4512e2ba1a9a789aa3646358cd187792b56b1443
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-01 02:15:45 +0530

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,7 @@ data_folder = 'data/meta_res_cp/'
 filename_list = os.listdir(data_folder)            
 algorithms = ['lightgbm', 'random_forest',          
               'libsvm_svc', 'extra_trees',              
-              'liblinear_svc', 'k_nearest_neighbors',    
+              'liblinear_svc', 'k_nearest_neighbors',     
               'logistic_regression',   
               'gradient_boosting', 'adaboost'] 
 algo_str = '|'.join(algorithms)
