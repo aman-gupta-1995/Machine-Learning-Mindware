67fd27c8ba96e6d63f7dae6e33b0cb810c5565ce
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-01 02:10:44 +0530

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,7 +5,7 @@ filename_list = os.listdir(data_folder)
 algorithms = ['lightgbm', 'random_forest', 
               'libsvm_svc', 'extra_trees',     
               'liblinear_svc', 'k_nearest_neighbors',   
-              'logistic_regression', 
+              'logistic_regression',  
               'gradient_boosting', 'adaboost'] 
 algo_str = '|'.join(algorithms)
 pattern = '(.*)_(%s)_(\d+)_(\d+)_20.pkl' % algo_str
