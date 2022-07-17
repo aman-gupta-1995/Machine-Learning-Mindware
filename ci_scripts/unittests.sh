225a80e38b104fa58cfbdfef647ba6b5d25317f1
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-18 00:07:59 +0530

    Update unittests.sh

--- ci_scripts/unittests.sh
+++ ci_scripts/unittests.sh
@@ -8,7 +8,7 @@ files=$(ls $folder)
 for file in $files       
 do
     echo "Begin to run $file" 
-    python $file
+    python $file 
     rval=$?
     if [ "$rval" != 0 ]; then
         echo "Error running example $file"
